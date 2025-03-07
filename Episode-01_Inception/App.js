24c533484dff00add7fa37fe4a93b6e1a67d9b7a
Rumit Varsani <varsanirumit@gmail.com>
2025-03-07 23:31:07 +0100

    Today i learn JSX

--- Episode-01_Inception/App.js
+++ Episode-01_Inception/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = (
-  <div>
+  const Title = ()=>(
+    <h1>hello title</h1>
+  );
+  const Name = () =>(
     <div>
-      <h1>I am an h1 tag</h1>
-      <h1>I am an h1 tag</h1>
+      <Title />
+      <h1>Hellow name</h1>
     </div>
-    <div>
-      <h1>I am an h1 tag</h1>
-      <h1>I am an h1 tag</h1>
-    </div>
-  </div>
-);
+    
+  );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Name/>);
